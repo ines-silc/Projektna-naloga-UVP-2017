a6493c3d7044293f06aca36e63c8dc22591496df
ines-silc <ines.silc@gmail.com>
2017-05-15 15:26:11 +0200

    Add files via upload

---
+++ vislice.py
@@ -0,0 +1,51 @@
+from turtle import *
+hideturtle()
+left(90)
+circle(45, 180)
+circle(45, -90)
+right(90)
+
+forward(150)
+right(90)
+
+forward(100)
+backward(60)
+right(135)
+
+forward(56)
+backward(56)
+left(135)
+forward(60)
+right(90)
+
+forward(40)
+right(90)
+
+circle(15)
+circle(15, 180)
+right(90)
+
+forward(60)
+backward(40)
+left(45)
+
+forward(30)
+backward(30)
+right(90)
+
+forward(30)
+backward(30)
+left(45)
+
+forward(40)
+left(45)
+
+forward(30)
+backward(30)
+right(90)
+
+forward(30)
+print('Konec igre!')
+
+
+
