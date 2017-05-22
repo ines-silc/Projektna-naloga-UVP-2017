ac826b2f74d46ca97d5a22da82cb0b7d25cfb2f0
Ines Šilc <ines.silc@student.fmf.uni-lj.si>
2017-05-22 18:19:40 +0200

    Začetek z grafičnim umesnikom

--- Vislice-program.py
+++ Vislice-program.py
@@ -28,15 +28,41 @@ razdeljena_beseda = razdeli_besedo(beseda)
 
 vzorec = izpisi_vzorec()
 
-def vzemi_crko():
-    vnesena_crka = input()
-    print(vnesena_crka)
-
-def spremeni_vzorec():
-    for x in razdeljena_beseda:
-        if vnesena_crka == x:
-            vzorec[x] = vnesena_crka
-        else:
-            print('neveljavno')
+def spremeni_vzorec(crka):
+    if str(crka) in razdeljena_beseda:
+        for i, value in enumerate(razdeljena_beseda):
+            if value == str(crka):
+                vzorec[i] = value.replace('__', str(crka))
+    else:
+        crka = nova_crka = input('Nova črka? ')
+        for i, value in enumerate(razdeljena_beseda):
+            if value == str(nova_crka):
+                vzorec[i] = value.replace('__', str(nova_crka))
     return vzorec
-        
+    
+import tkinter as tk
+
+okno = tk.Tk()
+polje = tk.Entry
+
+a = tk.Button(okno, text = 'A')
+a.pack()
+a.grid(row = 2, column = 1)
+b = tk.Button(okno, text = 'B')
+b.grid(row = 2, column = 2)
+c = tk.Button(okno, text = 'C')
+c.grid(row = 2, column = 3)
+d = tk.Button(okno, text = 'D')
+d.grid(row = 2, column = 4)
+e = tk.Button(okno, text = 'E')
+e.grid(row = 2, column = 5)
+f = tk.Button(okno, text = 'F')
+f.grid(row = 2, column = 6)
+g = tk.Button(okno, text = 'G')
+g.grid(row = 2, column = 7)
+h = tk.Button(okno, text = 'H')
+h.grid(row = 2, column = 8)
+i = tk.Button(okno, text = 'I')
+i.grid(row = 2, column = 9)
+okno.mainloop()
+
